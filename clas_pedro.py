cdabf53648fa4fdd1bd1790052805bc058828fde
Pedro Gómez <144271637+GRJPedro@users.noreply.github.com>
2025-01-07 18:13:07 -0600

    Create clas_pedro.py

---
+++ clas_pedro.py
@@ -0,0 +1,10 @@
+class Persono:
+  def __init__(mysillyobject, name, age):
+    mysillyobject.name = name
+    mysillyobject.age = age
+
+  def myfuncha(abc):
+    print("Hello my name is " + abc.name )
+
+p1 = Persono("John", 36)
+p1.myfuncha()
